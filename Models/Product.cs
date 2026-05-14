4f043fc24be10d0798c5f05b88519e1ef733f27f
MaksymMoskalenko <maksym.moskalenko.it@gmail.com>
2026-05-14 12:02:16 +0300

    Initiating ASP.NET project

---
+++ Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationForEnterprise.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Назва товару")]
+        public string Name { get; set; } = string.Empty;
+
+        [Display(Name = "Ціна")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Кількість на складі")]
+        public int QuantityInStock { get; set; }
+
+        [Display(Name = "Опис")]
+        public string? Description { get; set; }
+    }
+}

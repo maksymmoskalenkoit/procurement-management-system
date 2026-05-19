e13dc60fd3a39f3844f4bde72d6862ab4493313f
MaksymMoskalenko <maksym.moskalenko.it@gmail.com>
2026-05-19 13:48:33 +0300

    Changing view of pages

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationForEnterprise.ViewModels;
+
+namespace WebApplicationForEnterprise.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UsersController(
+            UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        // список користувачів
+        public IActionResult Index()
+        {
+            var users = _userManager.Users.ToList();
+
+            return View(users);
+        }
+
+        // GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(
+            CreateUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    EmailConfirmed = true
+                };
+
+                var result = await _userManager
+                    .CreateAsync(
+                        user,
+                        model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _userManager
+                        .AddToRoleAsync(
+                            user,
+                            model.Role);
+
+                    return RedirectToAction(nameof(Index));
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(
+                        "",
+                        error.Description);
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ ViewModels/CreateUserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationForEnterprise.ViewModels
+{
+    public class CreateUserViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Роль")]
+        public string Role { get; set; } = string.Empty;
+    }
+}
